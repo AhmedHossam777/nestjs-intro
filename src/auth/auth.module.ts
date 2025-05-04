842689d32528e583752ca271ba25b839b7b678a7
AhmedHossam777 <ahmedhossam9226@gmail.com>
2025-05-04 18:57:14 +0300

    feat[server] : implement hashing

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './providers/auth.service';
 import { UsersModule } from '../users/users.module';
+import { HashingProvider } from './providers/hashing.provider';
+import { BcryptProvider } from './providers/bcrypt.provider';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, HashingProvider, BcryptProvider],
   imports: [UsersModule],
 })
 export class AuthModule {}
